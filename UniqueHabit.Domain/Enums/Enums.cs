4044135a36e9b35265bb46c90dbd5373a8b912ce
Ashish Sabne <Ashish.Sabne@ghd.com>
2023-10-05 12:10:58 -0400

    Start fresh

--- UniqueHabit.Domain/Enums/Enums.cs
+++
@@ -1,8 +0,0 @@
-﻿public enum TemplateCategory
-{
-    Personal,
-    Career,
-    Relationships,
-    Health,
-    Other
-}
--- UniqueHabits.Shared/HabitState.cs
+++
@@ -1,9 +0,0 @@
-﻿using UniqueHabit.Domain.Aggregates;
-
-namespace UniqueHabits.Shared
-{
-    public class HabitState
-    {
-        public static Habit CurrentHabit { get; set; } = new Habit();
-    }
-}
